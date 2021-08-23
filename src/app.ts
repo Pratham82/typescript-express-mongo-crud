9a9940da057961dd18f3eba3de92d12d2a05c087
pratham82 <mali.prathamesh82@gmail.com>
2021-08-23 20:43:50 +0530

    ES6 style import added

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import connectToDB from './config/db'
 const app = express()
 
 const userRoutes = require('./routes/userRoutes')
+import router from './routes/userRoutes'
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
@@ -17,7 +18,7 @@ app.use(express.json())
 app.get('/', (_: Request, res: Response) => res.send('Express server started'))
 
 // User router
-app.use('/api/users', userRoutes)
+app.use('/api/users', router)
 
 // Start server
 app.listen(PORT, () =>
